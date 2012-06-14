cd932652e5a66368c89704fa9c672ab2f0585072
Mo Baig <mobaig@notonthehighstreet.com>
2012-06-14 14:22:09 +0100

    More graceful handling of CV links.

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -1,2 +1,16 @@
 module CandidatesHelper
+  def cv_link_helper(candidate)
+    if candidate.cv.path.blank?
+      'x'
+    else
+      link_to 'CV', download_cv_candidate_path(candidate)
+    end
+  end
+
+  def existing_cv_helper(candidate)
+    unless candidate.cv.path.blank?
+      cv_link = link_to("Existing CV: #{File.basename(candidate.cv.url)}", download_cv_candidate_path(candidate))
+      content_tag(:div, cv_link, :class => "existing_cv")
+    end
+  end
 end
