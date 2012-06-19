6189ee5da8c595f8361f82bf072880d502393cbc
Mo Baig <mobaig@notonthehighstreet.com>
2012-06-19 11:05:10 +0100

    Switched to mysql. Reduced number of example candidates in seeds file.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.2'
 
-gem 'sqlite3'
+gem 'mysql2'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-State.create(state: 'New')
-State.create(state: 'Interview Scheduled')
-State.create(state: 'Offered')
-State.create(state: 'Offer Declined')
-State.create(state: 'Rejected')
+# Candidate states:
+State.find_or_create_by_state(state: 'New')
+State.find_or_create_by_state(state: 'Interview Scheduled')
+State.find_or_create_by_state(state: 'Offered')
+State.find_or_create_by_state(state: 'Offer Declined')
+State.find_or_create_by_state(state: 'Rejected')
 
-
-20.times do
+# Example candidate:
+1.times do
   name = Faker::Name.name
   email = Faker::Internet.email
   Candidate.create(name: name, email: email)
